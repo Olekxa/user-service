16cb50226a7f33ef6cec8c49a7f352b1f40f554a
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-06-30 18:39:23 +0300

    GreedoBank-Kharkiv-T2 :Test

--- src/test/java/com/greedobank/reports/controller/NewsControllerTest.java
+++ src/test/java/com/greedobank/reports/controller/NewsControllerTest.java
@@ -1,7 +1,28 @@
 package com.greedobank.reports.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(NewsController.class)
 class NewsControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturn200WhenSendingRequestToController() throws Exception {
+        mockMvc.perform(get("/api/v1/news"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("GreedoBank completed Migration to Cloud!")));
+    }
 }
