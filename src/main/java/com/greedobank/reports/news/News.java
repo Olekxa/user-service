09c87763ad0220b6b7214ee255465c7802001467
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-07-08 22:16:31 +0300

    GreedoBank-Kharkiv-T13 : removed toString

--- src/main/java/com/greedobank/reports/news/News.java
+++ src/main/java/com/greedobank/reports/news/News.java
@@ -10,18 +10,4 @@ public record News(long id,
                    boolean active,
                    OffsetDateTime createdAt,
                    OffsetDateTime updatedAt) {
-
-    @Override
-    public String toString() {
-        return "{\n" +
-                "id: " + id + "\n" +
-                "displayOnSite: " + displayOnSite + "\n" +
-                "sendByEmail: " + sendByEmail + "\n" +
-                content + "\n" +
-                "publicationDate: " + publicationDate + "\n" +
-                "active: " + active + "\n" +
-                "createdAt: " + createdAt + ",\n" +
-                "updatedAt: " + updatedAt + ",\n" +
-                "}";
-    }
 }
