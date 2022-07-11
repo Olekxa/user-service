d26ef45f7a6dd7f306167987295f72d26ebf2549
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-07-11 11:29:57 +0300

    GreedoBank-Kharkiv-T15 : add storage for news

--- src/main/java/com/greedobank/reports/news/NewsPublic.java
+++ src/main/java/com/greedobank/reports/news/NewsPublic.java
@@ -4,18 +4,29 @@ import com.greedobank.reports.dto.NewsRequestDTO;
 import lombok.Getter;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.greedobank.reports.mapper.Mapper.mapToResponse;
 
 @Getter
 public class NewsPublic {
-    private long id = 0;
+    List<NewsDTO> newsPublic;
+    private long id;
+
+    public NewsPublic() {
+        this.newsPublic = new ArrayList<>();
+        id = 0;
+    }
 
     @ResponseBody
     public NewsDTO postNews(NewsRequestDTO request) {
         if (request.content().description() == null || request.content().title() == null) {
             throw new RuntimeException("description & title can't be empty");
         }
-        return mapToResponse(request, id++);
+        NewsDTO newsDTO = mapToResponse(request, id++);
+        newsPublic.add(newsDTO);
+        return newsDTO;
     }
 }
 
