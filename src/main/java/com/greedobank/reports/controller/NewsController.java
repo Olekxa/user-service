b61db632cc351934956a738986ac5487e99a7eee
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-07-13 17:38:32 +0300

    GreedoBank-Kharkiv-T13 : Init for function

--- src/main/java/com/greedobank/reports/controller/NewsController.java
+++ src/main/java/com/greedobank/reports/controller/NewsController.java
@@ -6,11 +6,11 @@ import com.greedobank.reports.dto.NewsResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -40,6 +40,29 @@ public class NewsController {
     @ResponseBody
     @Operation(summary = "Getting news", description = "get news by id")
     public NewsResponseDTO getNewsById(@PathVariable long id) {
+        OffsetDateTime timeCreate = OffsetDateTime.parse("2022-07-10T23:34:50.657873+03:00");
+        NewsResponseDTO newsResponseDTO = new NewsResponseDTO(1,
+                true,
+                true,
+                new ContentResponseDTO(
+                        "title",
+                        "description"),
+                OffsetDateTime.parse("2022-07-04T18:58:44Z"),
+                true,
+                timeCreate,
+                timeCreate);
+        if (id == newsResponseDTO.id()) {
+            return newsResponseDTO;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found news");
+        }
+    }
+
+    @PatchMapping(value = "/api/v1/news/{id}")
+    @Operation(summary = "Patch news", description = "patch news by id if present")
+    public NewsResponseDTO patchNewsById(
+            @PathVariable(value = "id") long id,
+            @RequestBody NewsResponseDTO newsRequestDTO) {
         OffsetDateTime timeCreate = OffsetDateTime.parse("2022-07-10T23:34:50.657873+03:00");
         NewsResponseDTO newsResponseDTO = new NewsResponseDTO(1,
                 true,
