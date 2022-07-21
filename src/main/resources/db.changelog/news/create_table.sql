0310fa7cdaa1798e5c8b22fd56f17296e1485b98
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-07-21 21:51:35 +0300

    GreedoBank-Kharkiv-T17 : Reports: Add sql script to create news table

---
+++ src/main/resources/db.changelog/news/create_table.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+--changeset greedobank:create-tables
+
+CREATE TABLE
+(
+    news_id INT PRIMARY KEY,
+    displayOnSite BOOLEAN,
+    sendByEmail BOOLEAN,
+    title VARCHAR,
+    description VARCHAR,
+    OffsetDateTime TIMESTAMP WITH TIME ZONE,
+    active BOOLEAN,
+    createdAt TIMESTAMP WITH TIME ZONE,
+    updatedAt TIMESTAMP WITH TIME ZONE
+);
