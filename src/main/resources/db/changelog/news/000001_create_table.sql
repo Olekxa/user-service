6cc5b3c8fdad049f3b6bc3d8a6f25c5260d43636
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-07-26 12:01:29 +0300

    GREEDO-BANK-T-17-fix creating of table

--- src/main/resources/db/changelog/news/000001_create_table.sql
+++ src/main/resources/db/changelog/news/000001_create_table.sql
@@ -6,9 +6,9 @@ CREATE TABLE news
     news_id INT PRIMARY KEY(),
     display_on_site BOOLEAN,
     send_by_email BOOLEAN,
-    title VARCHAR(NOT NULL),
-    description VARCHAR(NOT NULL),
-    offset_date_time TIMESTAMP WITH TIME ZONE(NOT NULL),
+    title VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    offset_date_time TIMESTAMP WITH TIME ZONE NOT NULL,
     active BOOLEAN,
     created_at TIMESTAMP WITH TIME ZONE,
     updated_at TIMESTAMP WITH TIME ZONE
