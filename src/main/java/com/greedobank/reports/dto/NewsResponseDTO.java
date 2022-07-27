4c109f48911108134cb0efdf80d6b889d690738f
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-07-27 11:22:16 +0300

    GREEDO-BANK-T-17

--- src/main/java/com/greedobank/reports/dto/NewsResponseDTO.java
+++ src/main/java/com/greedobank/reports/dto/NewsResponseDTO.java
@@ -4,9 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.OffsetDateTime;
 
 @Entity
+@Table
 public record NewsResponseDTO(
         @Id
         @GeneratedValue(strategy= GenerationType.AUTO)
