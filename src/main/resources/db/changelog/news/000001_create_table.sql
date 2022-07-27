1b4e72a65b2202bca4eac6e6580f8e016c5feffc
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-07-27 11:23:58 +0300

    GREEDO-BANK-T-17 fix field with null

--- src/main/resources/db/changelog/news/000001_create_table.sql
+++ src/main/resources/db/changelog/news/000001_create_table.sql
@@ -4,12 +4,12 @@
 CREATE TABLE news
 (
     news_id INT PRIMARY KEY,
-    display_on_site BOOLEAN,
-    send_by_email BOOLEAN,
+    display_on_site BOOLEAN NOT NULL,
+    send_by_email BOOLEAN NOT NULL,
     title VARCHAR NOT NULL,
     description VARCHAR NOT NULL,
     offset_date_time TIMESTAMP WITH TIME ZONE NOT NULL,
-    active BOOLEAN,
+    active BOOLEAN NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE,
     updated_at TIMESTAMP WITH TIME ZONE
 );
