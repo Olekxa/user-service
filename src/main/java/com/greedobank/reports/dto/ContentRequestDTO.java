38a225dfc994900b12536935f5ea80ab730c4cee
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-07-29 11:39:40 +0300

    GREEDO-BANK-T-22 intermediate

--- src/main/java/com/greedobank/reports/dto/ContentRequestDTO.java
+++ src/main/java/com/greedobank/reports/dto/ContentRequestDTO.java
@@ -1,6 +1,10 @@
 package com.greedobank.reports.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public record ContentRequestDTO(
+        @NotBlank
         String title,
+        @NotBlank
         String description) {
 }
