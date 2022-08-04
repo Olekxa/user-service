851dd6473f2cb2ba841d665e7b42eef80fa1c662
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-08-04 11:48:40 +0300

    Profile tested

---
+++ src/main/java/com/greedobank/reports/controller/HeathCheckController.java
@@ -0,0 +1,20 @@
+package com.greedobank.reports.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class HeathCheckController {
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/active-profiles")
+    @ResponseBody
+    public String[] healthCheck() {
+        return environment.getActiveProfiles();
+    }
+}
