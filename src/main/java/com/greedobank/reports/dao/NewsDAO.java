dc296b0e420c319a61f4bb5c2b25adae795922fc
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-08-08 11:11:55 +0300

    GREEDO-BANK-T22-fix issues

--- src/main/java/com/greedobank/reports/dao/NewsDAO.java
+++ src/main/java/com/greedobank/reports/dao/NewsDAO.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NewsDAO extends JpaRepository<News, Integer> {
+public interface NewsDAO extends JpaRepository<News, Long> {
 }
