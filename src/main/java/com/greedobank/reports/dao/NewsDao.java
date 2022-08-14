9a52eaf562866b9b8dcfb157422a096b143f3a94
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-08-14 18:03:14 +0300

    `GREEDO-BANK-Development

--- src/main/java/com/greedobank/reports/dao/NewsDao.java
+++
@@ -1,9 +0,0 @@
-package com.greedobank.reports.dao;
-
-import com.greedobank.reports.model.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NewsDAO extends JpaRepository<News, Long> {
-}
