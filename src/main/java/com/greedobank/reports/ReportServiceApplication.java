2d8a5a3d4e25efee0a6084870e3721d74d8f720e
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-08-14 17:43:42 +0300

    `GREEDO-BANK-T26 optimize import

--- src/main/java/com/greedobank/reports/ReportServiceApplication.java
+++ src/main/java/com/greedobank/reports/ReportServiceApplication.java
@@ -2,12 +2,10 @@ package com.greedobank.reports;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ReportServiceApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(ReportServiceApplication.class, args);
-
+        SpringApplication.run(ReportServiceApplication.class, args);
     }
 }
