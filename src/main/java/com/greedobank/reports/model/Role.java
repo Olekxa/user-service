cf2814d266eb775dcd010659d747b5896ba8f36e
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-08-24 23:24:47 +0300

    `GREEDO-BANK-T41

--- src/main/java/com/greedobank/reports/model/Role.java
+++ src/main/java/com/greedobank/reports/model/Role.java
@@ -1,9 +1,10 @@
 package com.greedobank.reports.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Role {
-    private int id;
     private RoleTitle title;
 }
--- src/main/java/com/greedobank/reports/model/User.java
+++ src/main/java/com/greedobank/reports/model/User.java
@@ -7,9 +7,6 @@ import lombok.Data;
 @AllArgsConstructor
 public class User {
         private int id;
-        private String firstName;
-        private String lastName;
         private String email;
-        private String password;
         private Role role;
 }
