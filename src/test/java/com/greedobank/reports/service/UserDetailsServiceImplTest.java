d28b779efd86678fdd8622723d7ca12aa2a8bb2b
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-09-22 00:01:27 +0300

    `GREEDO-BANK-T48 add new layer, changed test

--- src/test/java/com/greedobank/reports/service/UserDetailsServiceImplTest.java
+++ src/test/java/com/greedobank/reports/service/UserDetailsServiceImplTest.java
@@ -3,23 +3,19 @@ package com.greedobank.reports.service;
 import com.greedobank.reports.model.Role;
 import com.greedobank.reports.model.RoleTitle;
 import com.greedobank.reports.model.User;
+import com.greedobank.reports.model.UserWrapper;
 import com.greedobank.reports.utils.JwtUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.List;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -28,7 +24,7 @@ class UserDetailsServiceImplTest {
     private UserDetailsServiceImpl service;
 
     @Mock
-    private RestTemplate restTemplate;
+    private UserClient userClient;
 
     @Mock
     private JwtUtils jwtUtils;
@@ -36,33 +32,33 @@ class UserDetailsServiceImplTest {
     @BeforeEach
     private void setUp() {
         MockitoAnnotations.openMocks(this);
-        String url = "";
-        service = new UserDetailsServiceImpl(url, restTemplate, jwtUtils);
+        service = new UserDetailsServiceImpl(userClient, jwtUtils);
     }
 
     @Test
-    public void shouldReturnMail() {
+    public void shouldReturnUserWrapper() {
         String token = "";
         String mail = "dzhmur@griddynamics.com";
         User user = new User(1, "", "", mail, new Role(1, RoleTitle.ROLE_ADMIN));
         User[] array = {user};
-        ResponseEntity<User[]> responseEntity = new ResponseEntity<>(array, HttpStatus.ACCEPTED);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-        HttpEntity<String> entity = new HttpEntity<>(headers);
 
         when(jwtUtils.getEmail(token)).thenReturn(mail);
-        when(restTemplate.exchange(
-                ArgumentMatchers.anyString(),
-                ArgumentMatchers.any(HttpMethod.class),
-                ArgumentMatchers.any(),
-                ArgumentMatchers.<Class<User[]>>any()))
-                .thenReturn(responseEntity
-                );
+        when(userClient.buildRequest(mail)).thenReturn(array);
 
         UserDetails userDetails = service.loadUserByUsername(token);
         verify(jwtUtils, times(1)).getEmail(token);
-        verify(restTemplate, times(1)).exchange(mail, HttpMethod.GET, entity, User[].class);
+        verify(userClient, times(1)).buildRequest(mail);
+        assertEquals(new UserWrapper(user), userDetails);
+    }
 
+    @Test
+    public void shouldThrowException() {
+        String token = "";
+        String mail = "dzhmur@griddynamics.com";
+
+        when(jwtUtils.getEmail(token)).thenReturn(mail);
+        when(userClient.buildRequest(mail)).thenThrow(new UsernameNotFoundException("User not found with such email"));
+
+        assertThrows(UsernameNotFoundException.class, () -> service.loadUserByUsername(token));
     }
 }
