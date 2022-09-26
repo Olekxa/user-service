6e94066128f1e95ee641cfff621e2a4e26d25793
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-09-26 11:41:56 +0300

    GREEDO-BANK-T53 fix naming

--- src/main/java/com/greedobank/reports/controller/HealthCheckController.java
+++ src/main/java/com/greedobank/reports/controller/HealthCheckController.java
@@ -3,12 +3,11 @@ package com.greedobank.reports.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-public class HeathCheckController {
+public class HealthCheckController {
     @Autowired
     private Environment environment;
 
