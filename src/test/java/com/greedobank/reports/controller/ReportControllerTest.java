972a4ba56465b3a30e7b50dc65fb8dc3e4738b03
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-10-14 16:13:05 +0300

    GREEDO-BANK-T61 fix test

--- src/test/java/com/greedobank/reports/controller/ReportControllerTest.java
+++ src/test/java/com/greedobank/reports/controller/ReportControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +20,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ContextConfiguration
@@ -48,4 +50,18 @@ class ReportControllerTest {
         verify(mailService, times(1)).sendEmailWithAttachment(Mockito.any());
     }
 
+    @Test
+    @WithMockUser(username = "dzhmur@griddynamics.com", roles = "ADMIN")
+    public void shouldReturnReportByRequest() throws Exception {
+        Mockito.when(reportService.generateXlsxReport()).thenReturn(new byte[0]);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/report")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().bytes(new byte[0]));
+
+        verify(reportService, times(1)).generateXlsxReport();
+    }
 }
